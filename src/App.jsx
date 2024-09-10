4a0fe5cb4f4e2b2cca78d5bf1d473246f125a63b
Ahmad <rabieahmad330@gmail.com>
2024-09-10 23:23:33 +0300

    finished the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./Component/Header/Header";
 import Hero from "./Component/Hero/Hero";
+import "./App.css"
 function App() {
   return (
     <div className="App">
-      <>
-      <Header/>
-      <Hero/>
-      </>
-      
+      <div>
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 }
