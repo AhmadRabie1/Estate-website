7bc2334fbd52d1e9f091fb2ae89540b3f8cad3b3
Ahmad <rabieahmad330@gmail.com>
2024-09-15 23:34:26 +0300

    finished the footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Residencies from "./Component/Residencies/Residencies";
 import Value from "./Component/Value/Value";
 import Contact from "./Component/Contact/Contact";
 import GetStarted from "./Component/GetStarted/GetStarted";
+import Footer from "./Component/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -19,6 +20,7 @@ function App() {
       <Value/>
       <Contact/>
       <GetStarted/>
+      <Footer/>
     </div>
   );
 }
