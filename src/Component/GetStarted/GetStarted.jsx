758f1be6370b687bad97f31d1b56b67158d51b1d
Ahmad <rabieahmad330@gmail.com>
2024-09-15 23:09:57 +0300

    finished the GetStarted section

--- src/Component/GetStarted/GetStarted.jsx
+++ src/Component/GetStarted/GetStarted.jsx
@@ -2,8 +2,21 @@ import './GetStarted.css'
 
 const GetStarted = () => {
 
-    return(
-        <div>GetStarted</div>
+    return (
+        <section className="g-wrapper">
+            <div className="paddings innerWidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className='primaryText'>Get Started with Homyz</span>
+                    <span className='secondaryText'>Subscribe and find super attractive price quotes from us.
+                        <br/>
+                        Find your residence soon
+                    </span>
+                    <button className='button'>
+                        <a href="mailto:rabieahmad2009@gmail.com">Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
     );
 }
 export default GetStarted;
