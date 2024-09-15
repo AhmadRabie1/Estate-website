223e247d634fbc27c5f39dc335f3dd64023a3aa7
Ahmad <rabieahmad330@gmail.com>
2024-09-15 21:18:14 +0300

    finished the contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css"
 import Companies from "./Component/Companies/Companies";
 import Residencies from "./Component/Residencies/Residencies";
 import Value from "./Component/Value/Value";
+import Contact from "./Component/Contact/Contact";
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,7 @@ function App() {
       <Companies/>
       <Residencies/>
       <Value/>
+      <Contact/>
     </div>
   );
 }
